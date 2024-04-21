2b2c9714ea16b313e27b0e652707e60d44cade21
Laursad <167665720+Laursad@users.noreply.github.com>
2024-04-21 07:32:37 +0300

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Plane flight-0.1";
+const contentToCache = [
+    "Build/Plane flight (web).loader.js",
+    "Build/Plane flight (web).framework.js",
+    "Build/Plane flight (web).data",
+    "Build/Plane flight (web).wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
